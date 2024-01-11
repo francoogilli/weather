27bf253189b4f86452d954930954e5c0f6754c0f
Franco Gilli <francogilli10@gmail.com>
2024-01-11 16:50:03 -0300

    Adjusting the section container

--- src/components/DataMain.jsx
+++ src/components/DataMain.jsx
@@ -1,13 +1,14 @@
+// DataMain.jsx
 import React from 'react';
 import SectionContainer from './SectionContainer';
 
 const DataMain = () => {
   return (
-    <SectionContainer className={'py-10 pb-32 text-center grid grid-cols-2 md:grid-cols-4 gap-5 font-semibold text-4xl text-white items-center justify-center'}>
-      <div className='border border-white/20 rounded-2xl px-24 py-14 flex items-center justify-center '>30</div>
-      <div className='border border-white/20 rounded-2xl px-24 py-14 flex items-center justify-center '>30.9/21.3</div>
-      <div className='border border-white/20 rounded-2xl px-24 py-14 flex items-center justify-center '>38</div>
-      <div className='border border-white/20 rounded-2xl px-24 py-14 flex items-center justify-center '>11.2</div>
+    <SectionContainer className={'py-10 pb-32 text-center grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-2 font-semibold text-4xl text-white items-center justify-center'}>
+      <div className='border border-white/20 rounded-2xl px-8 md:px-16 lg:px-24 py-7 md:py-14 flex items-center justify-center '>30</div>
+      <div className='border border-white/20 rounded-2xl px-8 md:px-16 lg:px-24 py-7 md:py-14 flex items-center justify-center '>30.9/21.3</div>
+      <div className='border border-white/20 rounded-2xl px-8 md:px-16 lg:px-24 py-7 md:py-14 flex items-center justify-center '>38</div>
+      <div className='border border-white/20 rounded-2xl px-8 md:px-16 lg:px-24 py-7 md:py-14 flex items-center justify-center '>11.2</div>
     </SectionContainer>
   );
 };
