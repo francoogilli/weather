d5d46a73c1532f4429e420c449f2550d7bc526b4
Franco Gilli <francogilli10@gmail.com>
2024-01-11 17:12:53 -0300

    Adjusting padding in WidgetTemp

--- src/components/WidgetTemp.jsx
+++ src/components/WidgetTemp.jsx
@@ -14,7 +14,7 @@ const WidgetTemp = () => {
           <div className="col-span-1">
             {/* Contenido superior izquierdo */}
             <p className="text-7xl sm:text-[8.2rem] text-white pt-14 sm:pt-20 pl-0 sm:pl-0">19°</p>
-            <div className="pt-4 sm:pt-16 pl-1 sm:pl-0">
+            <div className="pt-1 sm:pt-16 pl-1 sm:pl-0">
               <p className="text-base sm:text-2xl text-[#AFA2D9] ">H:24° L:18°</p>
               <p className="text-base sm:text-2xl text-white ">Montreal, Canada</p>
             </div>
