eb3309e3c3d3527c53a6705a3e37b29871cb0b04
Franco Gilli <francogilli10@gmail.com>
2024-01-18 16:47:06 -0300

    Ajust view in mobile devices

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -20,19 +20,19 @@ const Details = ({sunrise, sunset, wind,winddir, pressure, snow, snowdepth}) => 
     <SectionContainer className={'py-0 pb-32 text-center grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-3 font-semibold text-3xl sm:text-4xl text-white items-center justify-center'}>
       <div  className='flex flex-col gap-y-4 bg-[#3b545c38] items-start border border-[#ffffff19] shadow-2xl rounded-3xl px-5 sm:px-9 py-5 '>
         <p className='flex items-center font-extrabold gap-x-1 text-sm text-[#ffffffb9]'><Sunrise/>SUNRISE</p>
-        <p className='text-white font-extrabold text-3xl sm:text-5xl '>{formatTime(sunrise)}<span className='text-xl sm:text-3xl'> AM</span></p>
+        <p className='text-white font-extrabold text-3xl sm:text-5xl '>{formatTime(sunrise)}<span className='text-lg sm:text-3xl'> AM</span></p>
         <p className='text-sm font-normal text-white text-left pt-5'>Sunset: {formatTime(sunset)} AM</p>
       </div>
 
       <div  className='flex flex-col gap-y-4 bg-[#3b545c38] items-start border border-[#ffffff19] shadow-2xl rounded-3xl px-5 sm:px-9 py-5 '>
         <p className='flex items-center font-extrabold gap-x-1 text-sm text-[#ffffffb9]'><Wind/>WIND</p>
         <p className='text-white font-extrabold text-4xl sm:text-5xl '>{Math.floor(wind)} <span className='text-2xl sm:text-3xl'>km/h</span></p>
-        <p className='text-sm font-normal text-white text-left'>The wind direction is {convertDir(winddir)}.</p>
+        <p className='text-sm font-normal text-white text-left'>The direction is {convertDir(winddir)}.</p>
       </div>
 
       <div  className='flex flex-col gap-y-4 bg-[#3b545c38] items-start border border-[#ffffff19] shadow-2xl rounded-3xl px-5 sm:px-10 py-5 '>
         <p className='flex items-center font-extrabold gap-x-1 text-sm text-[#ffffffb9]'><Pressure/>PRESSURE</p>
-        <p className='text-white font-extrabold text-4xl sm:text-5xl'>{Math.floor(pressure)} <span className='text-lg sm:text-3xl'>hPa</span></p>
+        <p className='text-white font-extrabold text-4xl sm:text-5xl'>{Math.floor(pressure)} <span className='text-base sm:text-3xl'>hPa</span></p>
         <p className='text-sm font-normal text-white text-left'>The pressure for the next hours.</p>
       </div>
 
