8beb06672fa86a09b111c74ff0823b1f5efc8547
Franco Gilli <francogilli10@gmail.com>
2024-01-19 11:07:57 -0300

    Added new loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const AnimatedDots = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-        <div className="loader border-t-[0.4em] border-[#d9dfff] bg-[#ffffff00]  w-[5.4em] h-[5.4em] rounded-full animate-spin"></div>
+        <div class="w-28 h-28 border-8 text-blue-400 text-4xl animate-spin border-[#ffffff9a] flex items-center justify-center border-t-blue-600 rounded-full"></div>
+    
+    
     </div>
   );
 };
